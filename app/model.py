31c87479d69b4d0b711e0e539da060edfc06b2c7
Snowman0045 <anuj46376.as@gmail.com>
2025-04-19 01:25:15 +0530

    vercel

--- app/model.py
+++ app/model.py
@@ -1,37 +1,39 @@
-import tensorflow as tf
 import numpy as np
-
-MODEL_PATH = "models/model.tflite"
+from huggingface_hub import hf_hub_download
+from tensorflow.keras.models import load_model
 
 lesion_type_dict = {
-    'nv': 'Melanocytic nevi',
-    'mel': 'Melanoma',
-    'bkl': 'Benign keratosis-like lesions ',
-    'bcc': 'Basal cell carcinoma',
-    'akiec': 'Actinic keratoses',
-    'vasc': 'Vascular lesions',
-    'df': 'Dermatofibroma'
+    "nv": "Melanocytic nevi",
+    "mel": "Melanoma",
+    "bkl": "Benign keratosis-like lesions",
+    "bcc": "Basal cell carcinoma",
+    "akiec": "Actinic keratoses",
+    "vasc": "Vascular lesions",
+    "df": "Dermatofibroma",
 }
+
 
 class ModelHandler:
     def __init__(self):
-        self.interpreter = tf.lite.Interpreter(model_path=MODEL_PATH)
-        self.interpreter.allocate_tensors()
+        model_path = hf_hub_download(
+            repo_id="sanchiittt/skin_cancer", filename="skin_cancer_model.h5"
+        )
+        self.model = load_model(model_path)
         self.class_keys = list(lesion_type_dict.keys())
 
     def predict(self, image_array: np.ndarray):
-        input_details = self.interpreter.get_input_details()
-        output_details = self.interpreter.get_output_details()
+        # Ensure input is 4D: (1, height, width, channels)
+        if len(image_array.shape) == 3:
+            image_array = np.expand_dims(image_array, axis=0)
 
-        self.interpreter.set_tensor(input_details[0]['index'], image_array)
-        self.interpreter.invoke()
-        output_data = self.interpreter.get_tensor(output_details[0]['index'])
+        output_data = self.model.predict(image_array)[0]
         predicted_index = np.argmax(output_data)
         label = self.class_keys[predicted_index]
         return {
             "predicted_class": label,
             "description": lesion_type_dict[label],
-            "confidence": float(output_data[0][predicted_index])
+            "confidence": float(output_data[predicted_index]),
         }
+
 
 model_handler = ModelHandler()
